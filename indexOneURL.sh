3dc3f9148b937a12c4883a819e546fdbbbacc050
Gregory Wiedeman <gw234478@lib-espy-ws-d101.its.albany.edu>
2019-03-06 16:43:44 -0500

    added script to index from github url

---
+++ indexOneURL.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+TYPE="$1"
+
+[ -s "/usr/local/rvm/scripts/rvm" ] && . "/usr/local/rvm/scripts/rvm"
+
+rvm 2.4.1@arclight
+
+echo "$(date) $line Indexing $TYPE.xml" >> /opt/lib/indexing-logs/index.log
+cd /var/www/arclight-UAlbany
+
+AREA=${TYPE//[0-9.]/}
+echo $AREA/$TYPE.xml
+
+bundle exec rake arclight:index_url URL=https://raw.githubusercontent.com/UAlbanyArchives/collections/master/$AREA/$TYPE.xml REPOSITORY_ID=$AREA SOLR_URL=https://solr.library.albany.edu:8984/solr/arclight-prod
