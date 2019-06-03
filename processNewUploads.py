6330794dd97d16793af1562cd8cdf0f25c18359a
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2019-06-03 15:07:34 -0400

    moved AIP class to dedicated repo

--- processNewUploads.py
+++ processNewUploads.py
@@ -5,14 +5,13 @@ import yaml
 import json
 import time
 import uuid
-import bagit
-import shutil
 import requests
 import shortuuid
 from bs4 import BeautifulSoup
 from datetime import datetime
 import asnake.logging as logging
 from asnake.client import ASnakeClient
+from packages import ArchivalInformationPackage
 client = ASnakeClient()
 client.authorize()
 
@@ -20,53 +19,6 @@ logging.setup_logging(stream=sys.stdout, level='INFO')
 requests.packages.urllib3.disable_warnings()
 
 __location__ = os.path.dirname(os.path.realpath(__file__))
-
-class ArchivalInformationPackage:
-
-    def __init__(self, colID, accession):
-        aipPath= "/media/Masters/Archives/AIP"
-        
-        metadata = {\
-        'Bag-Type': 'AIP', \
-        'Bagging-Date': str(datetime.now().isoformat()), \
-        'Posix-Date': str(time.time()), \
-        'BagIt-Profile-Identifier': 'https://archives.albany.edu/static/bagitprofiles/aip-profile-v0.1.json', \
-        }
-        
-        self.accession = accession
-        if not os.path.isdir(os.path.join(aipPath, colID)):
-            os.mkdir(os.path.join(aipPath, colID))
-        self.bagDir = os.path.join(aipPath, colID, accession)
-        os.mkdir(self.bagDir)
-        metadata["Bag-Identifier"] = accession
-        metadata["Collection-Identifier"] = colID
-        
-        self.bag = bagit.make_bag(self.bagDir, metadata)
-        self.data = os.path.join(self.bagDir, "data")
-        
-    def addMetadata(self, hyraxData):
-        headers = ["Type", "URIs", "File Paths", "Accession", "Collecting Area", "Collection Number", "Collection", \
-        "ArchivesSpace ID", "Record Parents", "Title", "Description", "Date Created", "Resource Type", "License", \
-        "Rights Statement", "Subjects", "Whole/Part", "Processing Activity", "Extent", "Language"]
-        metadataPath = os.path.join(self.bagDir, "metadata")
-        if not os.path.isdir(metadataPath):
-            os.mkdir(metadataPath)
-        metadataFile = os.path.join(metadataPath, accession + ".tsv")
-        addHeaders = False
-        if not os.path.isfile(metadataFile):
-            addHeaders = True
-        outfile = open(metadataFile, "a")
-        writer = csv.writer(outfile, delimiter='\t', lineterminator='\n')
-        if addHeaders == True:
-            writer.writerow(headers)
-        writer.writerow(hyraxData)
-        outfile.close()
-        
-    def addFile(file):
-        dataPath = os.path.join(self.bagDir, "data")
-        if not os.path.isdir(metadataPath):
-            os.mkdir(dataPath)
-        shutil.copy2(file, dataPath)
         
 def addASpaceDAO(refID, newURI, visibility):
     if visibility == "open":
