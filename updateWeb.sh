80e0ad349a98344e4883859b15ece658d92f69b4
gwiedeman <gregory.wiedeman1@gmail.com>
2020-01-03 16:07:28 -0500

    fixed update sh

--- updateWeb.sh
+++ updateWeb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-output=$( git --git-dir=/var/www/spe_website.git --work-tree=/var/www/spe_website fetch --dry-run 2>&1 )
+output=$( git --git-dir=/var/www/spe_website/git --work-tree=/var/www/spe_website fetch --dry-run 2>&1 )
 
 if [[ $output ]]; then
     git --git-dir=/var/www/spe_website/.git --work-tree=/var/www/spe_website fetch
