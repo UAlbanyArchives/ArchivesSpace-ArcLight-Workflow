95cb92a5d7a296fffcff7eea52b129221e0e4da3
gwiedeman <gregory.wiedeman1@gmail.com>
2020-01-03 15:31:53 -0500

    added script to auto build jekyll site from github

---
+++ updateWeb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+output=$( git --git-dir=/var/www/spe_website.git --work-tree=/var/www/spe_website fetch --dry-run 2>&1 )
+
+if [[ $output ]]; then
+    git --git-dir=/var/www/spe_website/.git --work-tree=/var/www/spe_website fetch
+    cd /var/www/spe_website && jekyll build
+else
+    true
+fi
