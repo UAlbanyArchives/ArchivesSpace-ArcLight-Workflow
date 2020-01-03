0d9c65833f2fb38245de2e4b642dc90c066da46c
gwiedeman <gregory.wiedeman1@gmail.com>
2020-01-03 16:18:29 -0500

    update script now uses rvm right

--- updateWeb.sh
+++ updateWeb.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
-output=$( git --git-dir=/var/www/spe_website/git --work-tree=/var/www/spe_website fetch --dry-run 2>&1 )
+output=$( git --git-dir=/var/www/spe_website/.git --work-tree=/var/www/spe_website fetch --dry-run 2>&1 )
 
 if [[ $output ]]; then
     git --git-dir=/var/www/spe_website/.git --work-tree=/var/www/spe_website fetch
+    [ -s "/usr/local/rvm/scripts/rvm" ] && . "/usr/local/rvm/scripts/rvm"
+    rvm 2.6.3@website
     cd /var/www/spe_website && jekyll build
 else
     true
