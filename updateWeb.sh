2f8c42b43a9649c74fdb634f4d44bec87c058f3a
gwiedeman <gregory.wiedeman1@gmail.com>
2020-01-03 16:24:12 -0500

    now it shoud work:

--- updateWeb.sh
+++ updateWeb.sh
@@ -3,7 +3,7 @@
 output=$( git --git-dir=/var/www/spe_website/.git --work-tree=/var/www/spe_website fetch --dry-run 2>&1 )
 
 if [[ $output ]]; then
-    git --git-dir=/var/www/spe_website/.git --work-tree=/var/www/spe_website fetch
+    git --git-dir=/var/www/spe_website/.git --work-tree=/var/www/spe_website pull
     [ -s "/usr/local/rvm/scripts/rvm" ] && . "/usr/local/rvm/scripts/rvm"
     rvm 2.6.3@website
     cd /var/www/spe_website && jekyll build
