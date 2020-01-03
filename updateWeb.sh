bc440d59c69dd2b52a42fa88f98557fcb4469824
gwiedeman <gregory.wiedeman1@gmail.com>
2020-01-03 16:25:37 -0500

    now it should

--- updateWeb.sh
+++ updateWeb.sh
@@ -2,7 +2,8 @@
 
 output=$( git --git-dir=/var/www/spe_website/.git --work-tree=/var/www/spe_website fetch --dry-run 2>&1 )
 
-if [[ $output ]]; then
+if [[ $output ]]
+then
     git --git-dir=/var/www/spe_website/.git --work-tree=/var/www/spe_website pull
     [ -s "/usr/local/rvm/scripts/rvm" ] && . "/usr/local/rvm/scripts/rvm"
     rvm 2.6.3@website
