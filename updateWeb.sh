6d4ec67375dddaf5e5e7e82fb08d36d6e7332921
gwiedeman <gregory.wiedeman1@gmail.com>
2021-11-23 16:21:00 -0500

    updated ruby version to match other apps

--- updateWeb.sh
+++ updateWeb.sh
@@ -6,7 +6,7 @@ if [[ $output ]]
 then
     git --git-dir=/var/www/spe_website/.git --work-tree=/var/www/spe_website pull
     [ -s "/usr/local/rvm/scripts/rvm" ] && . "/usr/local/rvm/scripts/rvm"
-    rvm 2.6.3@website
+    rvm 2.6.5@website
     cd /var/www/spe_website && jekyll build
     cp /var/www/spe_website/_site/index.html /var/www/hyrax-UAlbany/public
 else
