5d1ad92a9b118425ed68192afba337e359f2a4a0
gwiedeman <gregory.wiedeman1@gmail.com>
2022-02-16 13:21:04 -0500

    fixed all indexer

--- indexAllEAD.sh
+++ indexAllEAD.sh
@@ -4,17 +4,13 @@ TYPE="$1"
 CORE="$2"
 #VERSION="$3"
 
-
-#[ -s "/home/railsdev/.rvm/scripts/rvm" ] && . "/home/railsdev/.rvm/scripts/rvm"
+# The user running this script must have an arclight rvm gemset with the correct gems
+# clone the arclight-UAlbany repo into your home dir and run 'bundle install' from there
 [ -s "~/.rvm/scripts/rvm" ] && . "~/.rvm/scripts/rvm"
-
-export REPOSITORY_ID=$TYPE
-
 rvm 2.6.5@arclight
 
-#echo "$(date) $line Export $TYPE" #>> /media/SPE/indexing-logs/index-$VERSION.log
 cd ~/arclight-UAlbany
 
 find /media/SPE/collections/$TYPE -type f -name '*.xml' -exec \
-~/.rvm/gems/ruby-2.6.5@arclight/bin/traject -u https://solr2020.library.albany.edu:8984/solr/$CORE -i xml \
+bundle exec traject -u https://solr2020.library.albany.edu:8984/solr/$CORE -i xml \
 -c ~/.rvm/gems/ruby-2.6.5@arclight/bundler/gems/arclight-f9b61c2cf12c/lib/arclight/traject/ead2_config.rb {} ';'
