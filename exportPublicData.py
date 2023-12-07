1c2a5762b7c7d2f4262cad6a4e8f692696d322be
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2023-12-07 13:07:45 -0500

    hopefully fixed exports

--- exportPublicData.py
+++ exportPublicData.py
@@ -191,7 +191,7 @@ for colID in modifiedList:
                 print ("\t\t\tExporting EAD")
                 params = {"include_daos": True}
                 eadResponse = client.get("repositories/2/resource_descriptions/" + resourceID + ".xml", params=params)
-                xml = etree.parse.fromstring(eadResponse.text)
+                xml = etree.fromstring(eadResponse.text)
                 pretty_xml = etree.tostring(xml, pretty_print=True)
                 eadFile = os.path.join(eadDir, ID + ".xml")
                 f = open(eadFile, 'w', encoding='utf-8')
